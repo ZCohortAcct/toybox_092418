dda540add55cd93bcf6cd69192a3ad4497072375
NickisKnowledge <lifechangingstuff@gmail.com>
2019-02-06 14:55:42 -0600

    Create User & Toy models with associations. And seed file

---
+++ db/migrate/20190206203811_create_toys.rb
@@ -0,0 +1,12 @@
+class CreateToys < ActiveRecord::Migration[5.2]
+  def change
+    create_table :toys do |t|
+      t.string :name
+      t.string :size
+      t.string :theme
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
