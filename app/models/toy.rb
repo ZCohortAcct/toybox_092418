0f4d6e54ba5a9205247785739f438e7df785b0b4
NickisKnowledge <lifechangingstuff@gmail.com>
2019-02-27 11:55:02 -0600

    Add CONDITIONAL optional true to assoc, to allow seed file to function

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,6 +1,6 @@
 class Toy < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :toymaker
+  belongs_to :toymaker, optional: true
 
   validates :name, :size, :theme, presence: true
 
