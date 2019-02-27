9c3baa1cb94cdf2a248fa93b77ffa3779239a111
NickisKnowledge <lifechangingstuff@gmail.com>
2019-02-27 14:46:10 -0600

    Create and use toymaker_link helper

--- app/helpers/toys_helper.rb
+++ app/helpers/toys_helper.rb
@@ -1,2 +1,9 @@
 module ToysHelper
+  def toymaker_link(toy)
+    if toy.toymaker
+      link_to toy.toymaker.brand, toymaker_path(toy.toymaker)
+    else
+      link_to 'Add a Toymaker', toy_edit_path(toy)
+    end
+  end
 end
