2c76cf9f73e43c2770a41141fdb77ede397b2360
NickisKnowledge <lifechangingstuff@gmail.com>
2019-02-27 12:00:06 -0600

    Add toymakers to some toysd

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,6 @@ User.create(username: 'Mallory', password: '123')
 
 User.create(username: 'Joey', password: '123')
 
-
-toy_list = [
-  ['Thomas The Train','small', 'Trains', 1],
-  ['Maracas','medium', 'Music', 2],
-  ['Paw Patrol Race Track','large', 'Paw-Patrol', 1],
-  ['Doll House','large', 'Barbie', 2],
-  ['Bubbles','medium', 'Outside-Play',1],
-  ['Soccer ball','medium', 'Outside-Play', 2],
-  ['Guitar','small', 'Music', 2],
-  ['Thomas & Friends MINIS Motorized Raceway','medium', 'Trains', 1],
-  ['Marshall Plush toy','medium', 'Paw-Patrol', 1],
-]
-
-toy_list.each do |toy|
-  Toy.create!(name: toy[0], size: toy[1], theme: toy[2], user_id: toy[3])
-end
-
 toymakers = [
   ['Nickelodeon','action',42],
   ['Thomas-and-Friends Co.','train stuff',35],
@@ -42,3 +25,20 @@ toymakers = [
 toymakers.each do |co| 
   Toymaker.create(brand: co[0], kind: co[1], age: co[2])
 end
+
+toy_list = [
+  ['Thomas The Train','small', 'Trains', 1, 2],
+  ['Maracas','medium', 'Music', 2, 3],
+  ['Paw Patrol Race Track','large', 'Paw-Patrol', 1, 1],
+  ['Doll House','large', 'Barbie', 2],
+  ['Bubbles','medium', 'Outside-Play',1, 4],
+  ['Soccer ball','medium', 'Outside-Play', 2],
+  ['Guitar','small', 'Music', 2, 3],
+  ['Thomas & Friends MINIS Motorized Raceway','medium', 'Trains', 1, 2],
+  ['Marshall Plush toy','medium', 'Paw-Patrol', 1, 1],
+]
+
+toy_list.each do |toy|
+  Toy.create!(name: toy[0], size: toy[1], theme: toy[2], user_id: toy[3], toymaker_id: toy[4])
+end
+
