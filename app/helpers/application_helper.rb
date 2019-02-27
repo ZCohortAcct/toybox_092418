451df7352f51461192556a891b7820b8fdb3da93
NickisKnowledge <lifechangingstuff@gmail.com>
2019-02-27 14:36:07 -0600

    Display another way to render a partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def error_messages(obj)
-    render partial: 'application/error_msg', locals: {object: obj}
+    # render partial: 'application/error_msg', locals: {object: obj}
+    render 'application/error_msg', {object: obj}
   end
 end
