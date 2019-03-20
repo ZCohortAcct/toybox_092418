1cfa7a6832d0af2c2e5e380c72d4bbfd53beb2f2
NickisKnowledge <lifechangingstuff@gmail.com>
2019-03-20 01:00:03 -0500

    Create helper method to display message based on number of toys

--- app/helpers/toys_helper.rb
+++ app/helpers/toys_helper.rb
@@ -6,4 +6,13 @@ module ToysHelper
       link_to 'Add a Toymaker', toy_edit_path(toy)
     end
   end
+
+  def title_message(toys)
+    # byebug
+    if toys.length < 5
+      'This toybox has so few toys!'
+    else
+      'This toybox has a lot of toys!'
+    end
+  end
 end
