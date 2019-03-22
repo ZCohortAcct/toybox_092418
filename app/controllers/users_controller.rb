6d431a819f3515dbbc7f17e5d46f74fd4df01390
NickisKnowledge <lifechangingstuff@gmail.com>
2019-03-22 18:47:34 -0500

    Protected Users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: :show
+  before_action :authenticate_user!
+  before_action :set_user, only: :show
 
   def show
     # need to protect routes
-    @user = current_user
+      # binding.pry
+    redirect_to new_user_session_path if current_user.id != @user.id
     @logged_in_person = current_user
   end
 
-  # private
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
