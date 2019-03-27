9d1c20c11bf0bad779b39304b8987be57d2d0b85
NickisKnowledge <lifechangingstuff@gmail.com>
2019-03-27 14:59:38 -0500

    Build ToysUsers#create

--- app/controllers/toys_users_controller.rb
+++ app/controllers/toys_users_controller.rb
@@ -6,8 +6,18 @@ class ToysUsersController < ApplicationController
   end
 
   def create
+    # binding.pry
+    rental = ToysUser.new(toys_user_params)
+    # binding.pry
+    rental.save
+    redirect_to user_path(rental.user)
   end
 
   def update
+  end
+
+  private 
+  def toys_user_params
+    params.require(:toys_user).permit(:toy_rent_date, :functional, :toy_id,:user_id)
   end
 end
