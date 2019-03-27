ac85d69d387cd8b377739ec429bea87f384b90a0
NickisKnowledge <lifechangingstuff@gmail.com>
2019-03-27 17:41:25 -0500

    User can return a toy

--- app/controllers/toys_users_controller.rb
+++ app/controllers/toys_users_controller.rb
@@ -8,12 +8,15 @@ class ToysUsersController < ApplicationController
   def create
     # binding.pry
     rental = ToysUser.new(toys_user_params)
-    # binding.pry
     rental.save
     redirect_to user_path(rental.user)
   end
-
+  
   def update
+    # binding.pry
+    toys_user = ToysUser.find_by(toy_id: params[:toy_id], user_id: current_user.id)
+      toys_user.update(toy_return_date: Date.today, functional: false)
+      redirect_to user_path(current_user), {alert: "We hope you enjoyed playing with the #{toys_user.toy.name}"}
   end
 
   private 
