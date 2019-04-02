933c1d34df44645d1c62a1cb18620b082d82760c
NickisKnowledge <lifechangingstuff@gmail.com>
2019-04-01 22:29:09 -0500

    Changed scope methods to use 'toys_user' association

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -6,12 +6,14 @@ class Toy < ApplicationRecord
   validates :name, :size, :theme, presence: true
 
   def currently_rented?(user)
-    # t.toys_user.find_by(user_id: user.id)
-    ToysUser.toy_currently_rented?(self, user)
+    # ToysUser.toy_currently_rented?(self, user)
+    toys_user.where(toy_return_date: [nil, ''], user_id: user.id) != []
   end
 
   def past_rental_date(user)
-    ToysUser.past_toy_rentals(self, user)
+    # ToysUser.past_toy_rentals(self, user)
+    result = toys_user.where(user_id: user.id).where.not(toy_return_date: [nil, ''])
+    return result[0].toy_return_date if result != []
   end
 
   # def self.find_past_rentals(arr)
--- app/models/toys_user.rb
+++ app/models/toys_user.rb
@@ -2,30 +2,30 @@ class ToysUser < ApplicationRecord
   belongs_to :user
   belongs_to :toy
 
-  def self.toy_currently_rented?(toy, user)
+  # def self.toy_currently_rented?(toy, user)
     # ToysUser.where(toy_return_date: [nil, ''], toy_id: t2.id, user_id: u.id) != []
     # ToysUser.where(toy_return_date: [nil, ''], toy_id: t.id, user_id: u.id) != []
     # test_attrs = {
-    #   toy_id: toy.id, 
+    #   toy_id: toy.id,
     #   user_id: user.id
     # }
-    
+
     # test2_attrs = {
-    #   toy_id: t2.id, 
+    #   toy_id: t2.id,
     #   user_id: u.id
     # # }
     # ToysUser.where(test_attrs)[0].toy_return_date == nil
-    
-    where(toy_return_date: [nil, ''], toy_id: toy.id, user_id: user.id) != []
-  end
+
+    # where(toy_return_date: [nil, ''], toy_id: toy.id, user_id: user.id) != []
+  # end
 
 
-  def self.past_toy_rentals(toy, user)
-    foreign_keys = {toy_id: toy.id, user_id: user.id}
-    result = where(foreign_keys).where.not(toy_return_date: [nil, ''])
-    # binding.pry
-    return result[0].toy_return_date if result != []
-  end 
+  # def self.past_toy_rentals(toy, user)
+  #   foreign_keys = {toy_id: toy.id, user_id: user.id}
+  #   result = where(foreign_keys).where.not(toy_return_date: [nil, ''])
+  #   # binding.pry
+  #   return result[0].toy_return_date if result != []
+  # end 
 
   def self.user_past_rentals(user)
     where(user_id: user.id).where.not(toy_return_date: [nil, ''])
